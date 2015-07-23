0f2b31f9ed066265086372f4f846408770b473a6
Marc Laurent <dev.marc.laurent@gmail.com>
2015-07-23 02:21:49 -0300

    - Embedded functions related to contact in a extended contact list function.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,54 @@
 (function($, can) {
 	'use strict';
 
-	var contacts = new can.List([{
-		name: 'Fulano de Tal',
-		online: true
-	}, {
-		name: 'Beltrano',
-		online: false
-	}, {
-		name: 'Joãozinho',
-		online: true
-	}]);
-
-	var totalOnline = can.compute(function() {
-		var online = 0;
-
-		contacts.each(function(contact) {
-			if (contact.attr("online")) {
-				online++;
-			}
-		});
-
-		return online;
+	can.fixture({
+		'GET /contacts': function() {
+			return [{
+				id: 1,
+				name: 'Fulano de Tal',
+				online: true
+			}, {
+				id: 2,
+				name: 'Beltrano',
+				online: false
+			}, {
+				id: 3,
+				name: 'Joãozinho',
+				online: true
+			}];
+		}
 	});
 
+	can.fixture.delay = 1000;
+
+	var Contact = can.Model.extend({
+		findAll: 'GET /contacts'
+	});
+
+	Contact.List = Contact.List.extend({
+		filter: function(check) {
+			var list = new this.constructor;
+			this.each(function(contact) {
+				if (check(contact)) {
+					list.push(contact);
+				}
+			});
+			return list;
+		},
+		online: function() {
+			return this.filter(function(contact) {
+				return contact.attr('online');
+			});
+		},
+		onlineCount: function() {
+			return this.online().attr('length');
+		}
+	});
+
+	var contacts = new Contact.List({});
+
 	var frag = can.view('#dc-chat', {
-		contacts: contacts,
-		totalOnline: totalOnline
+		contacts: contacts
 	});
 
 	$('body').append(frag);
