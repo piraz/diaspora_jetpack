79679de7900df0ded3ab2562191ca04e12a18b6e
Flavio Garcia <piraz@candango.org>
2015-08-04 00:55:49 -0400

    Added jetpack suffix to the handler paths.

--- diaspora_jetpack/__init__.py
+++ diaspora_jetpack/__init__.py
@@ -8,9 +8,9 @@ class DiasporaJetpackComponent(firenado.core.TornadoComponent):
 
     def get_handlers(self):
         return [
-            (r'/', diaspora_jetpack.handlers.IndexHandler),
-            (r'/session', diaspora_jetpack.handlers.SessionHandler),
-            (r"/bower_components/(.*)", tornado.web.StaticFileHandler,
+            (r'/jetpack', diaspora_jetpack.handlers.IndexHandler),
+            (r'/jetpack/session', diaspora_jetpack.handlers.SessionHandler),
+            (r"/jetpack/bower_components/(.*)", tornado.web.StaticFileHandler,
              {"path": os.path.join(self.get_component_path(),
                                    'bower_components')}),
         ]
